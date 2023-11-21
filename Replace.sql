8e9b16e5a43d97a473784478afd76137e8fa5cb8
Rohan Kadi <118832951+rohankadi@users.noreply.github.com>
2023-11-21 22:11:11 +0530

    Add files via upload

---
+++ Replace.sql
@@ -0,0 +1,5 @@
+SELECT REPLACE(Hospital_name, 'Hospital', 'Medical Center') FROM Hospital;	
+
+SELECT REPLACE(Hospital_Location, 'Rd', 'Road') FROM HOSPITAL; 
+
+SELECT REPLACE('codnest', 'c', 'K');
