c5bae37ed95e972fc99c6378837166ebc377bcea
Rohan Kadi <118832951+rohankadi@users.noreply.github.com>
2023-11-21 22:10:07 +0530

    Add files via upload

---
+++ Hospital Table.sql
@@ -0,0 +1,39 @@
+Create table hospital (
+Hospital_code INT primary key,
+Hospital_name varchar(200),
+Hospital_location varchar(200) );
+
+INSERT INTO hospital (Hospital_code, Hospital_name, Hospital_location) VALUES
+(101, 'VS Hospitals', '#815/306, Poonamalle High Rd, Kilpauk, Chennai – 600010.'),
+(102, 'Apollo Hospitals', 'Plot # 13, Off Uran Road, Parsik Hill Rd, Sector 23, CBD Belapur, Navi Mumbai, Maharashtra 400614.'),
+(103, 'BLK Super Speciality Hospital', 'Pusa Rd, Radha Soami Satsang, Rajendra Place, New Delhi, Delhi 110005.'),
+(104, 'Max Super Speciality Hospital', 'Chandigarh Rd, near Civil Hospital, Phase 6, Sector 56, Sahibzada Ajit Singh Nagar, Punjab 160055.'),
+(105, 'Medanta Hospital', 'CH Baktawar Singh Rd, Medicity, Islampur Colony, Sector 38, Gurugram, Haryana 122001'),
+(106, 'Fortis Hospitals Group', '1, 01 Level 1, Bannerghatta Main Rd, opposite IIM, Bengaluru, Karnataka 560076.'),
+(107, 'Artemis Hospital', 'Sector 51, Gurugram, Haryana 122001.'),
+(108, 'Wockhardt Hospital', 'Wani House, Mumbai – Agra National Hwy, Wadala Naka, Nashik, Maharashtra 422001.'),
+(109, 'Narayana Health Hospital', 'Metro Station, Dharamshila marg, Vasundhara Enclave Near Ashok Nagar, Dallupura, New Delhi, Delhi 110096.'),
+(110, 'Columbia Asia Hospital', 'No. 85-86, Bangalore-Mysore Ring Road Junction, Bannimantapa ‘A’ Layout, Mandi Mohalla, Mysore, Karnataka – 570015.'),
+(111, 'Manipal Hospital', '98, Kodihalli, HAL Bus Stop'),
+(112, 'Sagar Hospitals', '44/54, 30th Cross, Tilaknagar, Jayanagar Extension, Opposite Indian Oil Petrol Bunk'),
+(113, 'People Tree Hospitals', '2, Tumkur Service Road, Goraguntepalya, Near BMTC Bus Stop And Opposite Taj Vivanta'),
+(114, 'Rxdx Healthcare', '7G, Ground Floor, Vishweshwaraiah Industrial Area, Whitefield Main Road, Opposite Graphite India'),
+(115, 'Sakra World Hospital', '52/2 and 52/3, Devarabeesanahalli Flyover, Kariyammana Agrahara, Outer Ring Road, Marathahalli, Near Passport Office and Opposite Intel'),
+(116, 'Columbia Asia Hospital', '26/4, Brigade Gateway, Malleshwaram West, Beside Metro Cash and Carry West'),
+(117, 'Bgs Gleneagles Global Hospital', '67, Uttarahalli Road, Near Abhiman Studio & SGBIT Engineering College Campus 1st Building'),
+(118, 'St John Medical College And Hospital', 'Koramangala 2 Block, Near Koramangala Water Tank'),
+(119, 'Dr. Parthasarathi s Hair And Skin Hospitals', 'No. 49, 1st Floor, 8th F Main, Jayanagar 3rd Block, Near ICICI Bank 3rd Block'),
+(120, 'Cloudnine Fertility - Ivf Centre, Electronic City', '1st Floor, Legacy Building, Neeladri Nagar, Electronics City Phase 1(inside Srjuana Hospital), Above Nilgirs & Opposite Ajmere Infinity Apartment'),
+(121, 'Vikram Hospital', '71/1, Vasanth Nagar, Opposite to St Annes College & Beside Navnit Motors'),
+(122, 'Prachi Healthcare', 'B4,5, Nester Raga, Outer Ring Road, Comercial Complex'),
+(123, 'Dr. Partha Sarathis Asian Hair And Skin Hospital', '5, 6, Sadath Court, Magrath Road, Opposite Garuda Mall'),
+(124, 'Excel Care Hospital', '3/2, 27th Cross, Monotype Road, K. R. Road Dead End, Next to Brigade Software Park'),
+(125, 'Kangaroo Care Women & Children Hospital', '89, 17th Cross, MC Layout, Near Balagangadharnath Swamy Metro Station'),
+(126, 'Cloudnine Hospital - Malleshwaram', '47, 17th Cross, 11th Main, Near MES College'),
+(127, 'Zilme Hospitals', '#33/1, Behind Mahatma Gandhi Statue, Horamavu.'),
+(128, 'Prachi Healthcare', 'B4,5, Nester Raga, Outer Ring Road, Comercial Complex'),
+(129, 'Brindhavvan Areion Hospital', '17, 4th Main Road, Rama Mandir Road, Near Tippu Sultan Palace'),
+(130, 'Sanjeevini Multispeciality Hospital', '761, 7th Main, Last Bus Stop, Mahalakshmipuram'),
+(131, ' Sanjeev Hospital ', 'Bengaluru');
+
+	
