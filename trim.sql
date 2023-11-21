d33ff0593e079262fe1755ce67d7dfdf3c54a3ac
Rohan Kadi <118832951+rohankadi@users.noreply.github.com>
2023-11-21 22:12:31 +0530

    Add files via upload

---
+++ trim.sql
@@ -0,0 +1,14 @@
+SELECT TRIM(' KODNEST ');
+
+SELECT TRIM(leading from '  KODNEST  '); 
+
+SELECT TRIM(trailing from '  KODNEST  ');
+
+SELECT TRIM(both from Hospital_name)
+FROM Hospital 
+WHERE Hospital_Code = 131;
+
+SELECT TRIM(Hospital_name)
+FROM Hospital 
+WHERE Hospital_Code = 131;
+
