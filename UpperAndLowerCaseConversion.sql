3e038bc24c88faa6815dbac8d595d75a04d6b16d
Rohan Kadi <118832951+rohankadi@users.noreply.github.com>
2023-11-21 22:12:43 +0530

    Add files via upload

---
+++ UpperAndLowerCaseConversion.sql
@@ -0,0 +1,17 @@
+USE Hospital;
+
+DESC Hospital;
+
+SELECT * FROM Hospital;
+
+SELECT UPPER(Hospital_name) FROM Hospital WHERE Hospital_Code = 128;
+
+SELECT UPPER('raja ram Mohan roy');
+
+SELECT
+LOWER(Hospital_Location) 
+FROM Hospital;
+
+SELECT LOWER(Hospital_Location) FROM Hospital WHERE Hospital_Code = 128;
+
+SELECT LOWER('KODNEST TECHNOLOGIES');
